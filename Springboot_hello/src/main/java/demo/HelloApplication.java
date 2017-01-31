da944a1b5753dd078750062ab9d6f660e4dfc48f
Tschuss <jiglesgom@gmail.com>
2017-01-31 23:24:03 +0100

    imports fixed

--- Springboot_hello/src/main/java/demo/HelloApplication.java
+++ Springboot_hello/src/main/java/demo/HelloApplication.java
@@ -2,10 +2,7 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Springboot_hystrix-dashboard/src/main/java/demo/HystrixDashboardApplication.java
+++ Springboot_hystrix-dashboard/src/main/java/demo/HystrixDashboardApplication.java
@@ -2,10 +2,8 @@ package demo;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
