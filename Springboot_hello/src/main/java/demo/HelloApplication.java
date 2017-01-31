38a6fabdcd5ac3c7280e32813fa187159a24d19e
Tschuss <jiglesgom@gmail.com>
2017-01-31 22:27:55 +0100

    v1

---
+++ Springboot_hello/src/main/java/demo/HelloApplication.java
@@ -0,0 +1,28 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class HelloApplication {
+
+  @RequestMapping("/hello")
+  public String home() {
+    return "Hello World";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApplication.class, args);
+  }
+
+}
