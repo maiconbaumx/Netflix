bc87a0e60cf98d0e260457e1192a12503d67528f
Tschuss <jiglesgom@gmail.com>
2017-02-03 00:08:53 +0100

    start script working!

--- Springboot_docker/src/demo/start.sh
+++ Springboot_docker/src/demo/start.sh
@@ -1,4 +1,14 @@
-/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_eureka/target/eurekaApplication-0.0.1-SNAPSHOT.jar&
-/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_zuul/target/zuulApplication-0.0.1-SNAPSHOT.jar&
-/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_hystrix-dashboard/target/HystrixDashboardApplication-0.0.1-SNAPSHOT.jar&
-/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_hello/target/helloApplication-0.0.1-SNAPSHOT.jar&
+#!/bin/bash
+echo starting...
+set $PATH = $PATH:$JAVA_HOME/bin
+echo start Eureka
+$JAVA_HOME/bin/java -jar /Netflix/Springboot_eureka/target/eurekaApplication-0.0.1-SNAPSHOT.jar &
+sleep 10
+echo start Zuul
+$JAVA_HOME/bin/java -jar /Netflix/Springboot_zuul/target/zuulApplication-0.0.1-SNAPSHOT.jar &
+sleep 10
+echo start Dashboard
+$JAVA_HOME/bin/java -jar /Netflix/Springboot_hystrix-dashboard/target/HystrixDashboardApplication-0.0.1-SNAPSHOT.jar &
+sleep 10
+echo start Hello
+$JAVA_HOME/bin/java -jar /Netflix/Springboot_hello/target/helloApplication-0.0.1-SNAPSHOT.jar
