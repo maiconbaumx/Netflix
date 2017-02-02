7bc6575eb640e3445a6aa4050cd192255ac55d4a
Tschuss <jiglesgom@gmail.com>
2017-02-02 19:43:13 +0100

    start all services

---
+++ Springboot_docker/src/demo/start.sh
@@ -0,0 +1,4 @@
+/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_eureka/target/eurekaApplication-0.0.1-SNAPSHOT.jar&
+/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_zuul/target/zuulApplication-0.0.1-SNAPSHOT.jar&
+/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_hystrix-dashboard/target/HystrixDashboardApplication-0.0.1-SNAPSHOT.jar&
+/usr/lib/jvm/java-8-oracle/bin/java -jar /Netflix/Springboot_hello/target/helloApplication-0.0.1-SNAPSHOT.jar&
