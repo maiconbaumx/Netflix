375004e3ec70327402e19e98b3291ef3d7702bef
Tschuss <jiglesgom@gmail.com>
2017-02-07 22:55:46 +0100

    sleep as param

--- Springboot_hello/src/main/java/demo/HelloApplication.java
+++ Springboot_hello/src/main/java/demo/HelloApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
@@ -14,14 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloApplication {
 
   @RequestMapping("/hello")
-  public String home() {
-    try {
-		Thread.sleep(500);
-	} catch (InterruptedException e) {
-		e.printStackTrace();
-	} 
-	return "Hello World";
-
+  public String home(@RequestParam("sleep") int time) {
+	if (time>0) {
+	    try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	return "Hello World\n";
   }
 
   public static void main(String[] args) {
