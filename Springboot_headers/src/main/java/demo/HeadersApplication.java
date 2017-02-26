e3ae91ca78cf0557740a6b66335c5948080f2a99
Tschuss <Tschuss@DESKTOP-9FGSP1C>
2017-02-26 13:44:42 +0100

    headers example

---
+++ Springboot_headers/src/main/java/demo/HeadersApplication.java
@@ -0,0 +1,29 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.util.MultiValueMap;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class HeadersApplication {
+
+  @RequestMapping("/hello")
+  public String home(@RequestHeader MultiValueMap<String,String> headers) {
+
+	return headers.toString();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeadersApplication.class, args);
+  }
+
+}
